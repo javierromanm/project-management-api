9a1a02907b79924b5b79b0ee61a19282dd014ddd
Javier Roman Minguez <javierroman@javierroman.com>
2024-06-13 15:18:43 +0200

    Part 26. Include login toute that returns an authentication token

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,32 @@ use App\Http\Controllers\DeveloperController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use App\Http\Middleware\UserIsAdmin;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', function(Request $request){
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)){
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    $token = $user->createToken('app-token')->plainTextToken;
+
+    return response()->json(['token' => $token]);
+    
+});
 
 Route::apiResource('/clients', ClientController::class)->middleware(['auth:sanctum', UserIsAdmin::class]);
 
